9f94d16e534a56c72885540e235fae32ffe17646
BrunoRijo <bruno_rijo@hotmail.com>
2022-11-06 14:11:09 -0300

    GetMapping, DeleteMapping, PostMapping

--- src/main/java/br/com/meuPrimeiroProjeto/meuProjetoSpring/controller/UserController.java
+++ src/main/java/br/com/meuPrimeiroProjeto/meuProjetoSpring/controller/UserController.java
@@ -3,7 +3,11 @@ package br.com.meuPrimeiroProjeto.meuProjetoSpring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.meuPrimeiroProjeto.meuProjetoSpring.model.User;
@@ -19,4 +23,18 @@ public class UserController {
 		return repository.findAll();
 	}
 
+	@GetMapping("/users/{username}")
+	public User getOneUser(@PathVariable("username") String username) {
+		return repository.findByUsername(username);
+	}
+	
+	@DeleteMapping("/users/delete/{id}")
+	public void deleteUser (@PathVariable("id") Integer id) {
+		repository.deleteById(id);
+	}
+	
+	@PostMapping("/users")
+	public void postUser(@RequestBody User user) {
+		repository.save(user);
+	}
 }
