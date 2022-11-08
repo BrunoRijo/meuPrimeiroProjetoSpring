2bc501507e7d5be63eb6e5bb3175b5f5c3d33404
BrunoRijo <bruno_rijo@hotmail.com>
2022-11-07 21:28:37 -0300

    API Rest Documentada com Swagger

--- src/main/java/br/com/meuPrimeiroProjeto/meuProjetoSpring/controller/UserController.java
+++ src/main/java/br/com/meuPrimeiroProjeto/meuProjetoSpring/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class UserController {
 	@Autowired
 	private UserRepository repository;
 	
-	@GetMapping("/users")
+	@GetMapping("")
 	public List<User> getUsers(){
 		return repository.findAll();
 	}
@@ -35,6 +36,11 @@ public class UserController {
 	
 	@PostMapping("/users")
 	public void postUser(@RequestBody User user) {
+		repository.save(user);
+	}
+	
+	@PutMapping()
+	public void putUser(@RequestBody User user) {
 		repository.save(user);
 	}
 }
